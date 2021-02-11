5342111f3d26baf078a58bbf71140c53d53a53f4
Jat Torres <jat.torres@gmail.com>
2021-02-11 21:24:26 +0800

    implemented sic data load

---
+++ src/main/java/com/janaldous/sponsorship/domain/CompanyHouseEntry.java
@@ -0,0 +1,68 @@
+package com.janaldous.sponsorship.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+/**
+ * Source: Company House https://developer-specs.company-information.service.gov.uk/companies-house-public-data-api
+ * @author janaldous
+ *
+ */
+@Entity
+@Data
+public class CompanyHouseEntry {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long companyNumber;
+	
+	@Column(name = "company_name")
+	private String companyName;
+	
+	private String etag;
+	
+	@Column(name = "previous_company_names")
+	private String previousCompanyNames;
+	
+	@Column(name = "address_line_1")
+	private String addressLine1;
+	
+	@Column(name = "address_line_2")
+	private String addressLine2;
+	
+	@Column(name = "address_care_of")
+	private String addressCareOf;
+	
+	@Column(name = "address_country")
+	private String addressCountry;
+	
+	@Column(name = "address_locality")
+	private String addressLocality;
+	
+	@Column(name = "address_po_box")
+	private String addressPOBox;
+	
+	@Column(name = "address_post_code")
+	private String adddressPostCode;
+	
+	@Column(name = "address_premises")
+	private String addressPremises;
+	
+	@Column(name = "address_region")
+	private String addressRegion;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<SIC> sicCodes;
+	
+}
--- src/main/java/com/janaldous/sponsorship/domain/SIC.java
+++ src/main/java/com/janaldous/sponsorship/domain/SIC.java
@@ -1,7 +1,5 @@
 package com.janaldous.sponsorship.domain;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,28 +7,16 @@ import javax.persistence.Id;
 
 import lombok.Data;
 
-/**
- * Sponsor from https://uktiersponsors.co.uk/
- * @author janaldous
- */
 @Entity
 @Data
-public class PDFSponsor {
+public class SIC {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String companyName;
+	private Long code;
 	
-	private String town;
-	
-	private String industry;
-	
-	private String mainTier;
-	
-	private String subTier;
-	
-	private Date dateAdded;
+	private String description;
 
 }
