d81898763ab4d809f7e529fce4a1c5197a431593
Jat Torres <jat.torres@gmail.com>
2021-03-10 00:05:02 +0800

    improvements

--- src/main/java/com/janaldous/sponsorship/dto/model/CompanySponsorDto.java
+++ src/main/java/com/janaldous/sponsorship/dto/model/CompanySponsorDto.java
@@ -5,18 +5,25 @@ import java.time.LocalDateTime;
 import com.janaldous.sponsorship.domain.core.FetchDataStatus;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.experimental.Accessors;
 
 @Data
+@NoArgsConstructor
 @Accessors(chain = true)
 public class CompanySponsorDto {
 	
+	@NonNull
 	private Long id;
 	
+	@NonNull
 	private PDFSponsorDto pdfSponsor;
 	
+	@NonNull
 	private CompanyHouseEntryDto companyHouseEntry;
 	
+	@NonNull
 	private FetchDataStatus fetchDataStatus;
 	
 	private LocalDateTime dateUpdated;
@@ -24,5 +31,7 @@ public class CompanySponsorDto {
 	private Boolean nameMatches;
 	
 	private Boolean localityMatches;
+	
+	private Boolean checked;
 
 }
