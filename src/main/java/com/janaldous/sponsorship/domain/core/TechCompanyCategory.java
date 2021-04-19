699b2e86b02eb77581ebcaad5ca91f1062dd56e4
Jat Torres <jat.torres@gmail.com>
2021-04-20 00:16:40 +0800

    added new company category

--- src/main/java/com/janaldous/sponsorship/domain/core/TechCompanyCategory.java
+++ src/main/java/com/janaldous/sponsorship/domain/core/TechCompanyCategory.java
@@ -1,5 +1,5 @@
 package com.janaldous.sponsorship.domain.core;
 
 public enum TechCompanyCategory {
-	CLOUD, SOFTWARE_DEV, MEDTECH, FINTECH;
+	CLOUD, SOFTWARE_DEV, MEDTECH, FINTECH, AUTOMATION, AI;
 }
