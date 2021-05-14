514706f6ba5bd7c423fbb6d358644e3f1884ca1e
Jat Torres <jat.torres@gmail.com>
2021-05-15 00:46:13 +0800

    updated search queries file

--- search_queries2.sql
+++ search_queries2.sql
@@ -23,4 +23,20 @@ WHERE CS.NAME_MATCHES IS TRUE
 * Process data
 * - Map London tech companies postcode to nearest TFL station -> TFL zone
 * - Be able to filter companies by TFL zone and important areas (City of London, Canary Wharf, Scilicon Roundabout)
+*/ -- Compare name from pdf sponsor and name from company house sponsor
+
+SELECT CHE.COMPANY_NAME as "Company House Name",
+	PS.COMPANY_NAME as "PDF Sponsor Name"
+FROM COMPANY_SPONSOR CS
+JOIN PDFSPONSOR PS ON CS.PDF_SPONSOR_ID = PS.ID
+JOIN COMPANY_HOUSE_ENTRY CHE ON CS.COMPANY_HOUSE_ENTRY_ID = CHE.ID
+WHERE CS.NAME_MATCHES IS NOT TRUE
+				AND PS.INDUSTRY = 'Computer Programming'
+				AND UPPER(PS.TOWN) like '%LONDON%'
+LIMIT 5;
+
+/**
+* TODO to clean data: 
+*	- normalize: add new column, normalized company name, to company house name and pdf sponsor name
+*   ltd -> limited
 */
